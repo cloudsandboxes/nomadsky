9a4a9d0c38cbde97c1f4557cf332d91d949909b7
mikekrom1 <52241579+mikekrom1@users.noreply.github.com>
2026-01-19 19:13:14 +0100

    Update app2.py

--- code/nomadsky-engine/UI/app2.py
+++ code/nomadsky-engine/UI/app2.py
@@ -21,7 +21,7 @@ def run_script():
     source = data.get('source')
     destination = data.get('destination')
     vmname = data.get('vmname')
-    # parameters = data.get('parameters')
+    parameters = data.get('parameters')
     
     # Path to your scripts
     script_path = f'C:/projects/nomadsky/code/nomadsky-engine/scripts/{script_name}'  
