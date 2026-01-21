fdb1358c238dadc3308e961c0ce0376825fc216e
mikekrom1 <52241579+mikekrom1@users.noreply.github.com>
2026-01-21 14:11:46 +0100

    Update config.py

--- code/Microsoft/config.py
+++ code/Microsoft/config.py
@@ -2,10 +2,12 @@ tenantid = "78ba35ee-470e-4a16-ba92-ad53510ad7f6"
 exportdisktype = "vhd"
 importdisktype = "vhd"
 number = 10
+
 # import osdisk variables
 storage_account_name = "compliceert20"
 location = "westeurope"
 container_name = "vhds"
 blob_name = "osdisk.vhd"
-subscription_id ="41aff5e1-41c9-4509-9fcb-d761d7f33740"
+subscription_id = "41aff5e1-41c9-4509-9fcb-d761d7f33740"
 resource_group = "output"
+destionationtenantid = "78ba35ee-470e-4a16-ba92-ad53510ad7f6"
