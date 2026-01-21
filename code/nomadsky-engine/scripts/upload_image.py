42ce8158aa0720b4cd562daf23a6d3dad7055740
mikekrom1 <52241579+mikekrom1@users.noreply.github.com>
2026-01-21 13:30:36 +0100

    Update upload_image.py

--- code/nomadsky-engine/scripts/upload_image.py
+++ code/nomadsky-engine/scripts/upload_image.py
@@ -9,6 +9,12 @@ source = sys.argv[1]
 destination = sys.argv[2]
 vmname = sys.argv[3]
 
+
+
+sys.path.append(r"C:/projects/nomadsky/code/microsoft-connections")
+
+
+
 # Extract specific value from previous files
 shared_data_json = sys.argv[4]  # 4th argument
 shared_data = json.loads(shared_data_json)
@@ -23,6 +29,30 @@ except IndexError:
 tenant_id = "78ba35ee-470e-4a16-ba92-ad53510ad7f6"
 #credential = InteractiveBrowserCredential(tenant_id=tenant_id)
 
+
+      sys.path.append(r"C:/projects/nomadsky/code/microsoft-connections")
+      import config
+      importdisktype = config.importdisktype
+      if (importdisktype == exportdisktype):
+            result = {
+             'message': f"the diskfile type is already '{config.importdisktype}' so no need to transform type!",
+             }
+            print(json.dumps(result))
+      else:
+            result = {
+             'message': f"the import diskfile type is different '{config.importdisktype}' to the export type '{exportfiletype}' so need to transform!",
+             }
+            print(json.dumps(result))
+      
+      
+elif destination == 'aws':
+   a='empty'
+   #     # AWS boto3 code to find VM
+   # etc.
+
+
+#from helpers import my_function
+#result = my_function(5)
 
 
 #add logic
