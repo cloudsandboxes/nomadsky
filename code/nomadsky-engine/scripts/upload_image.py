07e169a0c3eb76bed5441bc6f098461ffa2aa709
mikekrom1 <52241579+mikekrom1@users.noreply.github.com>
2026-01-21 13:35:12 +0100

    Update upload_image.py

--- code/nomadsky-engine/scripts/upload_image.py
+++ code/nomadsky-engine/scripts/upload_image.py
@@ -7,44 +7,27 @@ import json
 # Get arguments
 source = sys.argv[1]
 destination = sys.argv[2]
-vmname = sys.argv[3]
+vmname = sys.argv[3].lower()
+shareddata_json = sys.argv[4]
+shared_data = json.loads(shareddata_json)
 
+exportdisktype = shared_data.get('exportdisktype', '')
 
-
-sys.path.append(r"C:/projects/nomadsky/code/microsoft-connections")
-
-
-
-# Extract specific value from previous files
-shared_data_json = sys.argv[4]  # 4th argument
-shared_data = json.loads(shared_data_json)
-vm_resource_id = shared_data.get('resource_id', '')
-
-try:
-    subscription_id = vm_resource_id
-except IndexError:
-        raise Exception(f" Invalid resource ID format: '{vm_resource_id}' ")
-        
-# Authenticate interactively
-tenant_id = "78ba35ee-470e-4a16-ba92-ad53510ad7f6"
-#credential = InteractiveBrowserCredential(tenant_id=tenant_id)
-
-
+if destination == 'azure':
+      # Azure SDK code to find VM
       sys.path.append(r"C:/projects/nomadsky/code/microsoft-connections")
       import config
+      from upload-image import upload_image
       importdisktype = config.importdisktype
-      if (importdisktype == exportdisktype):
+    
+      try:
             result = {
              'message': f"the diskfile type is already '{config.importdisktype}' so no need to transform type!",
              }
             print(json.dumps(result))
-      else:
-            result = {
-             'message': f"the import diskfile type is different '{config.importdisktype}' to the export type '{exportfiletype}' so need to transform!",
-             }
-            print(json.dumps(result))
-      
-      
+      except IndexError:
+        raise Exception(f" Invalid resource ID format: '{shared_data}' ")
+   
 elif destination == 'aws':
    a='empty'
    #     # AWS boto3 code to find VM
@@ -53,18 +36,6 @@ elif destination == 'aws':
 
 #from helpers import my_function
 #result = my_function(5)
-
-
-#add logic
-
-
-
-result = {
-   'message': f"VM '{vmname}' has been uploaded successfully to '{destination}'!",
-   'resource_id': vm_resource_id
-    }
-print(json.dumps(result))
-
 
 
 
