7a1fe6ff6cfe187d6bd6c1b7dcaab9cafb17ad37
mikekrom1 <52241579+mikekrom1@users.noreply.github.com>
2026-01-22 12:39:16 +0100

    Update create_vm.py

--- code/Microsoft/create_vm.py
+++ code/Microsoft/create_vm.py
@@ -5,45 +5,20 @@ def start_vm (shared_data):
   from azure.mgmt.compute import ComputeManagementClient
   from azure.mgmt.network import NetworkManagementClient
   from azure.mgmt.resource import ResourceManagementClient
-  import tkinter as tk
-  from tkinter import simpledialog, messagebox
   import config
   import re
-
-  root = tk.Tk()
-  root.withdraw()  # hide main window
-
-  username = simpledialog.askstring("Credentials", "Enter username:")
   
-  while True:
-      password = simpledialog.askstring("Password", "Enter a password:", show="*")
-      if password is None:  # user cancelled
-          break
-
-      # Check rules: min 15 chars, at least 1 uppercase, 1 number, 1 special
-      if (len(password) >= 15 and
-          re.search(r"[A-Z]", password) and
-          re.search(r"[0-9]", password) and
-          re.search(r"[!@#$%^&*(),.=+-_?\":{}|<>]", password)):
-          messagebox.showinfo("Success", "Password accepted!")
-          break
-      else:
-          messagebox.showerror("Invalid", "Password must be at least 15 characters with uppercase, number, and special character.")
-  
-
-  #print(f"Username: {username}, Password: {password}")
-
   subscription_id = config.subscription_id
   resource_group = config.resource_group
   vm_name = shared_data.get('vm_name', '')
   location = config.location
-  account_url = shared_data.get('url', '')
+  account_url = shared_data.get('disk_url', '')
   vhd_url = account_url + "/" + config.container_name + "/" + config.blob_name
   nic_id = shared_data.get('nic_id', '')
   os_type = shared_data.get('os_type', '')
   vm_size = shared_data.get('vm_size', '')
 
-  vhd_url = 'https://compliceert20.blob.core.windows.net/vhds/osdisk.vhd'
+  #vhd_url = 'https://compliceert20.blob.core.windows.net/vhds/osdisk.vhd'
   
   tenant_id = config.destionationtenantid
   credential = InteractiveBrowserCredential(tenant_id=tenant_id)
@@ -73,12 +48,6 @@ def start_vm (shared_data):
           ]
       }      
   }
-
-  #"os_profile": {
-  #        "computer_name": vm_name,
-  #        "admin_username": username,
-  #        "admin_password": password  # for Windows/Linux
-  #    }
   
   async_vm_creation = compute_client.virtual_machines.begin_create_or_update(resource_group, vm_name, vm_params)
   async_vm_creation.wait()
