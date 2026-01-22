b1ae1feddc0c80e948e9fb3e4095ef1ec795f240
mikekrom1 <52241579+mikekrom1@users.noreply.github.com>
2026-01-23 00:04:38 +0100

    Update config.py

--- code/Google/config.py
+++ code/Google/config.py
@@ -5,3 +5,5 @@ zone = "europe-west1-b"  # Optional: specify zone, or None to search all
 gcs_bucket = "vm-export-bucket"
 download_path = "C:/temp"
 credentials_path = "C:/path/to/service-account-key.json"
+importdisktype= "VMDK"
+exportdisktype= "VMDK"
