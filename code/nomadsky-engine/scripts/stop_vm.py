cd3ba3259ada90d975082e7514d4db29961a0818
mikekrom1 <52241579+mikekrom1@users.noreply.github.com>
2026-01-22 14:36:28 +0100

    Update stop_vm.py

--- code/nomadsky-engine/scripts/stop_vm.py
+++ code/nomadsky-engine/scripts/stop_vm.py
@@ -1,26 +1,39 @@
 import sys
-import subprocess
-# Simulate fetching VM
-# TODO: Add actual API calls to source platform here
+import json
 
 # Get arguments
 source = sys.argv[1]
 destination = sys.argv[2]
 vmname = sys.argv[3].lower()
 shareddata_json = sys.argv[4]
+shared_data = json.loads(shareddata_json)
 
 if source == 'azure':
       # Azure SDK code to find VM
-      result = subprocess.run(
-        ['python', 'C:/Projects/nomadsky/code/Microsoft/stop_vm.py', source, destination, vmname, shareddata_json],
-        capture_output=True,
-        text=True,
-        check=True
-      )
-      output = result.stdout
-      print(result.stdout)
-      # Get the output
+      sys.path.append(r"C:/projects/nomadsky/code/Microsoft")
+      import config
+      from stopping_vm import stop_vm
+          
+      try:
+            result = stop_vm(shared_data)
+            result = {
+             'message': f"the diskfile is succesfully transfered to '{destination}' at the '{url}'",
+             'disk_url' : url
+             }
+            print(json.dumps(result))
+      except IndexError:
+        raise Exception(f" Invalid resource ID format: '{shared_data}' ")
+   
 elif source == 'aws':
    a='empty'
    #     # AWS boto3 code to find VM
    # etc.
+
+
+#from helpers import my_function
+#result = my_function(5)
+#exportdisktype = shared_data.get('exportdisktype', '')
+#a, b = my_function(10)
+
+
+
