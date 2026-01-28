678d5cda4b5fced7a14e3e086c20499c175a94de
mikekrom1 <52241579+mikekrom1@users.noreply.github.com>
2026-01-28 15:10:12 +0100

    Create config.py

---
+++ code/Cyso/config.py
@@ -0,0 +1,3 @@
+# configuation parameters for Cyso.cloud or referenced by the old name: https://my.fuga.cloud/account/api-endpoints
+
+OS_APPLICATION_CREDENTIAL_ID = '33730d2e61274dd584f0d7b2fa846fba')
