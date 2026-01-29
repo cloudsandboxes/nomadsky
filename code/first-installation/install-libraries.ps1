1449a1bc6e9821554fb9e5d7e42f462a1a3990ac
mikekrom1 <52241579+mikekrom1@users.noreply.github.com>
2026-01-29 13:55:12 +0100

    Create install-libraries.ps1

---
+++ code/first-installation/install-libraries.ps1
@@ -0,0 +1,33 @@
+py -m pip install pywebview
+py -m pip install flask flask-cors
+py -m pip install opencensus-ext-azure
+
+#runnning azure
+py -m pip install azure-identity azure-mgmt-compute
+py -m pip install azure-mgmt-resource
+py -m pip install requests
+py -m pip install azure-storage-blob
+py -m pip install azure-mgmt-storage
+py -m pip install  azure-core
+py -m pip install  azure-mgmt-network
+py -m pip install azure-mgmt-compute
+
+#running GCP Google
+py -m pip install google-cloud-compute
+py -m pip install google-cloud-storage
+py -m pip install google-auth
+
+#running huawei
+py -m pip install huaweicloudsdkcore
+py -m pip install huaweicloudsdkecs
+py -m pip install huaweicloudsdkims
+py -m pip install huaweicloudsdkevs
+py -m pip install huaweicloudsdkobs
+
+# running aws
+py -m pip install boto3
+
+# running openstack
+
+py -m pip install python-novaclient
+py -m pip install python-glanceclient
