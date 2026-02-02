f57badc72d1566b387a70730074b012e54b8cf76
mikekrom1 <52241579+mikekrom1@users.noreply.github.com>
2026-02-02 12:01:46 +0100

    Update stopping_vm.py

--- code/Cyso/stopping_vm.py
+++ code/Cyso/stopping_vm.py
@@ -55,7 +55,7 @@ def stop_vm():
     root.destroy()
 
     auth = ApplicationCredential(
-     auth_url=os.environ.get('OS_AUTH_URL', 'https://core.fuga.cloud:5000/v3'),
+     auth_url=os.environ.get('OS_AUTH_URL', config.sourcecloudurl),
      application_credential_id=config.OS_APPLICATION_CREDENTIAL_ID,
      application_credential_secret= password
     )
