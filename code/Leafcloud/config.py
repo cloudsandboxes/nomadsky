64a882c121ad46b622730a71de5f203b076382f1
mikekrom1 <52241579+mikekrom1@users.noreply.github.com>
2026-02-02 15:40:23 +0100

    Update config.py

--- code/Leafcloud/config.py
+++ code/Leafcloud/config.py
@@ -1,1 +1,12 @@
+# configuation parameters for Leaf.cloud
 
+# Your application credential is to login to your cloud environment 
+OS_APPLICATION_CREDENTIAL_ID = '6e064903743147188cf917074e71d06c'
+sourcecloudurl = "https://create.leaf.cloud:8000/v1"  # location of the current cloud environment
+
+
+
+
+# Destination parameters:
+nics = [{"net-id": "496c99b9-4ae0-4cde-b648-d7412832b81b"}]  #network id
+destinationcloudurl = "https://create.leaf.cloud:8000/v1"  # location of the current cloud environment either Amsterdam or frankfurt https://fra.fuga.cloud:5000/v3
