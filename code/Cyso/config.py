cd2fd382cddc95d69df42b3c4d631257a740774c
mikekrom1 <52241579+mikekrom1@users.noreply.github.com>
2026-02-02 11:56:10 +0100

    Update config.py

--- code/Cyso/config.py
+++ code/Cyso/config.py
@@ -2,7 +2,11 @@
 
 # Your application credential is to login to your cloud environment 
 OS_APPLICATION_CREDENTIAL_ID = '33730d2e61274dd584f0d7b2fa846fba'
+sourcecloudurl = "https://core.fuga.cloud:5000/v3"  # location of the current cloud environment either Amsterdam or frankfurt https://fra.fuga.cloud:5000/v3
 
-# destination parameters:
+
+
+
+# Destination parameters:
 nics = [{"net-id": "496c99b9-4ae0-4cde-b648-d7412832b81b"}]  #network id
-
+destinationcloudurl = "https://core.fuga.cloud:5000/v3"  # location of the current cloud environment either Amsterdam or frankfurt https://fra.fuga.cloud:5000/v3
