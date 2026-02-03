8d36b94d0aa01a6500cfe77ecf74a768f83c6a0a
mikekrom1 <52241579+mikekrom1@users.noreply.github.com>
2026-02-03 12:20:38 +0100

    Update upload_disk.py

--- code/Cyso/upload_disk.py
+++ code/Cyso/upload_disk.py
@@ -14,7 +14,7 @@ def uploading_disk(vm_name):
     from keystoneauth1.identity import v3
     import getpass
     import json
-    sys.path.append(r"C:/projects/nomadsky/code/Cyso")
+    sys.path.append(r"C:/projects/digitalnomadsky/code/Cyso")
     import tkinter as tk
     from tkinter import simpledialog
     import time
