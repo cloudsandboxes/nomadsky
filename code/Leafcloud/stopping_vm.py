e186f0485267552045f08698bede247e22f1d437
mikekrom1 <52241579+mikekrom1@users.noreply.github.com>
2026-02-03 12:21:15 +0100

    Update stopping_vm.py

--- code/Leafcloud/stopping_vm.py
+++ code/Leafcloud/stopping_vm.py
@@ -13,7 +13,7 @@ def stop_vm():
     from keystoneauth1.identity import v3
     import getpass
     import json
-    sys.path.append(r"C:/projects/nomadsky/code/Leafcloud")
+    sys.path.append(r"C:/projects/digitalnomadsky/code/Leafcloud")
     import tkinter as tk
     from tkinter import simpledialog
     import time
